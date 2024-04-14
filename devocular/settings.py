dd961f7499d1106c945f8373c73c824700706bda
Alae <alae1ajbar@gmail.com>
2024-04-14 02:29:16 +0100

    settings

--- devocular/settings.py
+++ devocular/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-*j0m_9in=tn58=6$@r3x)-s$&ce@mzax%xxpql_8bb#=qsbw7g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
